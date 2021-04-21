cdfa90ad4c7a31ee97f11cd894eb87bf2cf657b2
amairani20blanco <79120043+amairani20blanco@users.noreply.github.com>
2021-04-21 00:14:29 -0500

    Update complexity-problems.js

--- complexity-problems.js
+++ complexity-problems.js
@@ -10,7 +10,25 @@
 // -> a integer
 // -> b integer
 // <- lcm 
-
+let h=1;  
+function mcm (a,b){
+let i=2;
+while(i<=a || i<=b){
+if(a%i==0 || b%i==0){
+  h*=i;
+ 	if(a%i==0)
+    a=a/i;
+  if(b%i==0)  
+    b=b/i;
+  }
+  else
+  i++;
+}
+return h;  
+}
+let a=prompt("Ingrese un numero "),b=prompt("Ingrese otro numero ");
+let c= mcm(a,b);
+document.write( " minimo comun  multiplo  " + c );  
 //2. sieve of Eratosthenes
 //   sieve(n)
 // -> n integer
@@ -20,9 +38,32 @@
 //   random(n)
 // -> n integer
 // <- one random number in [1-n]
+<script>
 
+function Num_ale()
+{ 
+let y=Math.random()*20;
+return Math.floor (1+y);
+}
+let a=Num_ale();
+
+document.write("Numero aleatorio " + a );
+ 
+</script>
 //4. random name
 //   randomName()
 // -> 
 // <- one random name
 // ie name371
+ <script>
+
+function Num_ale()
+{ 
+let y=Math.random()*40;
+return Math.floor (1+y);
+}
+let a=Num_ale();
+
+document.write("Nombre anónimo " + a );
+ 
+</script>
