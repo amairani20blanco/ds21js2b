467f14913feb473e872514c980cd326e20ed6fe6
amairani20blanco <79120043+amairani20blanco@users.noreply.github.com>
2021-04-21 00:18:05 -0500

    Update sorting-problems.js

--- sorting-problems.js
+++ sorting-problems.js
@@ -12,12 +12,48 @@
 // -> d an array of random numbers
 // <- the random numbers sorted
 // verify is sorted
+function Num_ale()
+{ 
+let y=Math.random()*40;
+return Math.floor (1+y);
+}
+let a=[];
+for(i=0;i<6;i++)
+{
+  let c=Num_ale();
+  a[i]=c;
+}
+document.write("Numeros aleatorios: "+ a+"<br>");  
+for (let k = 1; k < a.length; k++) {
+        for (let i = 0; i < a.length - k; i++) {
+            if (a[i] > a[i + 1]) {
+                let alm = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = alm;
+            }
+        }
+    }  
+document.write("Numeros ordenados: " + a) 
 
 //2. sort - use JS Array.sort implementation
 //   sort(d)
 // -> d an array of random numbers
 // <- the random numbers sorted
 // verify is sorted
+function Num_ale()
+{ 
+let y=Math.random()*40;
+return Math.floor (1+y);
+}
+let a=[];
+for(i=0;i<6;i++)
+{
+  let c=Num_ale();
+  a[i]=c;
+}
+document.write(a+"<br>");  
+
+document.write(a.sort((a,b)=>a-b)); 
 
 //3. bubble - your implementation 
 //   sort(o)
